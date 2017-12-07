353d7b5ffde056cefd9408e956dc7f8d8c4e27b6
wangxu <xuwanga@ctrip.com>
2017-12-07 21:48:41 +0800

    加入 MTA

---
+++ mta.js
@@ -0,0 +1,9 @@
+var _mtac = {};
+(function() {
+    var mta = document.createElement("script");
+    mta.src = "http://pingjs.qq.com/h5/stats.js?v2.0.2";
+    mta.setAttribute("name", "MTAH5");
+    mta.setAttribute("sid", "500559759");
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(mta, s);
+})();
